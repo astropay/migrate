1856f76285da8f5ec14cfe15b447f93e3aa2d5a0
robert <robert@astropay.com>
2019-10-16 12:42:10 -0300

    added optional parameter host

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,7 @@ import (
 
 var (
 	db             string
+	dbHost         string
 	dbEnv          string
 	dbUser         string
 	dbPassword     string
@@ -34,10 +35,12 @@ func main() {
 		} else {
 			log.Fatal(err.Error())
 		}
+
+		return
 	}
 
 	// run migration command
-	if err := runMigrate(db, dbEnv, dbUser, dbPassword, command, migrationsPath); err != nil {
+	if err := runMigrate(db, dbHost, dbEnv, dbUser, dbPassword, command, migrationsPath); err != nil {
 		log.Fatal(err.Error())
 	} else {
 		log.Println("OK!")
@@ -48,6 +51,7 @@ func main() {
 func parseArguments() (printUsage bool, err error) {
 
 	flag.StringVar(&db, "db", "", "database schema migrations to run")
+	flag.StringVar(&dbHost, "dbhost", "", "database host to connect; if present, 'env' is ignored")
 	flag.StringVar(&dbEnv, "env", "", "environment where connecting in order to run the migrations: prod, stage, test")
 	flag.StringVar(&dbUser, "dbuser", "", "database user to run the migration scripts")
 	flag.StringVar(&dbPassword, "dbpass", "", "database password to run the migration scripts")
@@ -63,20 +67,26 @@ func parseArguments() (printUsage bool, err error) {
 
 func checkArguments() (printUsage bool, err error) {
 
+	// check environment
+	if dbHost == "" {
+		dbEnv = strings.ToLower(dbEnv)
+		switch dbEnv {
+		case "test", "prod", "staging", "dev":
+			break
+		case "":
+			return true, fmt.Errorf("invalid parameter [env] value: '%s'", dbEnv)
+		default:
+			return false, fmt.Errorf("invalid parameter [env] value: '%s'", dbEnv)
+		}
+	}
+
+	if dbEnv == "" && dbHost == "" {
+		return false, errors.New("must indicate [host or [env] parameters")
+	}
+
 	// database
 	if db == "" {
 		return false, errors.New("invalid argument db")
-	}
-
-	// check environment
-	dbEnv = strings.ToLower(dbEnv)
-	switch dbEnv {
-	case "test", "prod", "staging", "dev":
-		break
-	case "":
-		return true, fmt.Errorf("invalid parameter [env] value: '%s'", dbEnv)
-	default:
-		return false, fmt.Errorf("invalid parameter [env] value: '%s'", dbEnv)
 	}
 
 	// check user and password
@@ -101,5 +111,7 @@ func checkArguments() (printUsage bool, err error) {
 func printUsage() {
 	fmt.Print("AstroPay Migration Tool\n\n")
 	flag.Usage()
-	fmt.Print("\nexample: migrate -db=apc -env=test -dbuser=myuser -dbpass=Passw0rd -command=up\n")
+	fmt.Print("\nexamples:\n")
+	fmt.Print("\tmigrate -env=test -db=apc -dbuser=myuser -dbpass=Passw0rd -command=up\n")
+	fmt.Print("\tmigrate -host=127.0.0.1:3306 -db=apc -dbuser=myuser -dbpass=Passw0rd -command=up\n")
 }
